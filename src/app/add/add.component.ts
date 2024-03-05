2bb1cc1b8a8c69c8a0cd36acb09aa01818f47cf6
Jmini00 <jerome27@me.com>
2024-03-05 14:45:13 +0100

    mise à jour

---
+++ src/app/add/add.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Pokemon } from '../models/pokemon';
+import { LISTDEPOKEMON } from '../db/list.pokemons';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent {
+  allPokemons = LISTDEPOKEMON;
+  newPokemon: Pokemon = new Pokemon();
+  
+  constructor(private router:Router) { }
+
+
+
+  addPokemon() {
+    this.newPokemon.id = this.allPokemons.length + 1;
+    this.allPokemons.unshift(this.newPokemon);
+    this.router.navigate(['']);
+  }
+
+
+}
